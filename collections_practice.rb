694fc37dda24bc87ef2ebe90285ed7387123887e
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-16 23:10:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,3 +39,12 @@ def sum_array(array)
   array.sum
 end
 
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      “#{element}s”
+  end
+  end
+end
